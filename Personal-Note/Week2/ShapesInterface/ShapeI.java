d6ddb0dddf95fc3bead23a7bfe7c5d17338da2f9
Louis Nguyen <ndmn004@gmail.com>
2023-10-08 14:15:35 +1100

    Initial upload

---
+++ Personal-Note/Week2/ShapesInterface/ShapeI.java
@@ -0,0 +1,13 @@
+package ShapesInterface;
+
+public interface ShapeI {
+
+    public double area();
+
+    public double circumference();
+
+    default public void printArea() {
+        System.out.println(" (from ShapeI interface, default method printArea)> area is: " + this.area());
+    }
+
+}
